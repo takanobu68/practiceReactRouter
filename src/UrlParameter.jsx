2a3e7088c2650fd5b1c1ee708f4639f3cafbdeae
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-06-07 16:12:40 +0900

    クエリパラメータ

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,10 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+
   return (
     <div>
       <h1>UrlParameter</h1>
+      <p>クエリパラメータは{query.get("name")}</p>
     </div>
   );
 };
